bb08e2cd96ae8dfe1c377678a44bfc73ec2b0d46
ajinakaya <ajina.kayastha5@gmail.com>
2025-07-20 00:58:28 +0545

    featured

---
+++ frontend_veloura/src/components/featured.jsx
@@ -0,0 +1,45 @@
+// NewArrivals.jsx
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Productcards from '../components/cards';
+
+const Featured = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/jewelry/tag/Featured");
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center py-16">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#B88E2F]"></div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Productcards 
+        products={products} 
+        headline="Trending" 
+        subheading="Visit Styelo to see amazing creations from our designers."
+        tag="Featured"
+      />
+    </div>
+  );
+};
+
+export default Featured;
