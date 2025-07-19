3c6d06782404909ee96f47be4898a31bff61b7fc
ajinakaya <ajina.kayastha5@gmail.com>
2025-07-20 00:59:59 +0545

    recommendation

---
+++ frontend_veloura/src/components/Recommendation.jsx
@@ -0,0 +1,45 @@
+// NewArrivals.jsx
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Productcards from '../components/cards';
+
+const Recommended = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/jewelry/tag/Recommended");
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center py-16">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#B88E2F]"></div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Productcards 
+        products={products} 
+        headline="Recommended" 
+        subheading="Visit Styelo to see amazing creations from our designers."
+        
+      />
+    </div>
+  );
+};
+
+export default Recommended;
